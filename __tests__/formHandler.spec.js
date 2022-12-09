a9726b6679850420f0af9312947f06e98ab9b7dd
Chris Shields <94402111+Shields003@users.noreply.github.com>
2022-12-09 10:24:53 -0600

    Delete __tests__ directory

--- __tests__/formHandler.spec.js
+++
@@ -1,8 +0,0 @@
-import { handleSubmit } from '../client/js/formHandler';
-
-describe('Get Sentiment Analysis function', () => {
-    // https://jestjs.io/docs/en/expect#tobedefined
-    test('Check that the handleSubmit function is not undefined', () => {
-        expect(handleSubmit).toBeDefined();
-    });
-});
