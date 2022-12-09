f0b0d188c520a4ca5f2248b1696c5e2ac547bd0c
Chris Shields <94402111+Shields003@users.noreply.github.com>
2022-12-09 09:53:11 -0600

    Add files via upload
    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Capstone Project
+
+
+
+## Installating and Running the app
+First, `npm install` 
+
+## .Env file
+Next, you will need to update the '.env' with your API credentials
+
+### Development environment
+Then run the `build-dev` script to run webpack-dev-server with development config
+Finally, run the `start` script to start the server
+
+## Technologies used
+* Supertest
+* Axios
+* Workbox
+* Webpack
+* Jest
+* Express
+
+## API's used
+
+### GeoNames
+<https://www.geonames.org/export/web-services.html>
+
+
+### Weatherbit API
+<https://www.weatherbit.io/account/dashboard>
+
+
+### Pixabay
+ <https://pixabay.com/api/docs/>
+
---
+++ src/client/app.js
@@ -0,0 +1,23 @@
+import { inputValidate } from './js/inputValidate';
+import { numDays } from './js/numDays';
+import { cardInfo } from './js/cardInfo';
+import { savedTrips } from './js/savedTrips';
+import { handleSubmit, saveTrip, removeTrip } from './js/formHandler';
+import { getGeonameInfo } from './js/getGeonameInfo';
+import { getWeatherBitInfo } from './js/getWeatherBitInfo';
+import { getPixabayImages } from './js/getPixabayImages';
+
+import './styles/index.scss';
+
+export {
+    inputValidate,
+    numDays,
+    cardInfo,
+    savedTrips,
+    handleSubmit,
+    saveTrip,
+    removeTrip,
+    getWeatherBitInfo,
+    getGeonameInfo,
+    getPixabayImages,
+};
